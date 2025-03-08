892f6b149be5c806582d9417c7fdfd145429ce17
SantiagoAHS <lionelsanti2018@gmail.com>
2025-03-07 19:57:06 -0600

    08 Book property

--- djangobnb_backend/property/urls.py
+++ djangobnb_backend/property/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', api.properties_list, name='properties_list'),  # Ruta para obtener todas las propiedades
     path('create/', api.create_property, name='api_create_property'),
     path('<uuid:pk>/', api.properties_detail, name='api_properties_detail'),
+    path('<uuid:pk>/book/', api.book_property, name='api_book_property'),
+    path('<uuid:pk>/reservations/', api.property_reservations, name='api_property_reservations'),
     
 ]
